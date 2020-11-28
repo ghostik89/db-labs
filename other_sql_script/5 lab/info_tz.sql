e9bb84924f5db1229914187ad1a1f417c184e3c5
Lady_Proger <facktartar@mail.com>
2020-11-28 22:51:47 +0400

    create all transaction sql scripts. start info script

---
+++ other_sql_script/5 lab/info_tz.sql
@@ -0,0 +1,10 @@
+---1. show comments to product
+SELECT * FROM mydb.comments where (`user_owns_product_ ID` = '1');
+
+---2. show product
+select * from `mydb`.`products` where (`ID` = '1');
+
+---3. show article
+select * from `mydb`.`article` where (`ID` = '1');
+
+---4. show all products
