985e001d2095ddc17da97fedcacd3ca0bbfe7690
Lady_Proger <facktartar@mail.com>
2020-11-30 21:54:56 +0400

    complete analyze changes

---
+++ other_sql_script/5 lab/analyze.sql
@@ -0,0 +1,26 @@
+---19. show all by category id
+SELECT * FROM mydb.products where (`category_ID` = 1);
+
+---20. show all by auditory id
+SELECT * FROM mydb.products where (`auditory_of_games_ID` = 2);
+
+---21. show all by published house id
+SELECT * FROM mydb.products where (`published_houses_ID` = 2);
+
+---22. show all products in favorite list in user
+select * from `mydb`.`favorite_products` where (`user_ID` = '1');
+
+---23. show all products in busket by user id
+select * from `mydb`.`users_order_products` where (`user_ID` = '1') and (`product_state_ID` = '6');
+
+---24. show all products by owner id
+select * from `mydb`.`user_owns_product` where (`user_ID` = '4');
+
+---25. show all products on way by user id
+SELECT * FROM mydb.user_owns_product 
+left join users_order_products on
+user_owns_product.user_ID = users_order_products.user_owns_product_ID
+where (user_owns_product.user_ID = 4);
+
+---26. show all articles
+SELECT * FROM mydb.article;
--- other_sql_script/5 lab/info_tz.sql
+++ other_sql_script/5 lab/info_tz.sql
@@ -1,11 +1,11 @@
----1. show comments to product
+---15. show comments to product
 SELECT * FROM mydb.comments where (`user_owns_product_ ID` = '1');
 
----2. show product
+---16. show product
 select * from `mydb`.`products` where (`ID` = '1');
 
----3. show article
+---17. show article
 select * from `mydb`.`article` where (`ID` = '1');
 
----4. show all products
+---18. show all products
 SELECT * FROM mydb.products;
