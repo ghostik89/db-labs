a5549a5d61dd4eaea50cd2d29c9c82e73c0551fb
Lady_Proger <facktartar@mail.com>
2020-12-14 01:27:25 +0400

    complete funcs and view for 5 lab

--- other_sql_script/5 lab/create_funcs.sql
+++ other_sql_script/5 lab/create_funcs.sql
@@ -54,19 +54,57 @@ END$$
 DELIMITER ;
 
 ---2. count all user owns product by id
----todo update this!!!
 DELIMITER $$
 CREATE FUNCTION countAllOwnsProductsByUserId(
 	_id int
 ) 
-RETURNS int
+RETURNS varchar(40)
 DETERMINISTIC
 BEGIN
     DECLARE counter int;
-	SELECT count(COUNT) into counter FROM mydb.user_owns_product where user_ID = _id;
-	RETURN (counter);
+    declare message varchar(40) default 'This user haven\t products';
+    
+	SELECT count(*) into counter FROM mydb.user_owns_product where user_ID = _id;
+    if counter > 0 then
+		set message = concat('This user has ', counter,' products');
+    end if;
+	RETURN (message);
 END$$
 DELIMITER ;
 
 
 ---3. calc bill for users basket
+DELIMITER $$
+CREATE FUNCTION createBillForUserById(
+	_id int,
+    _state int
+) 
+RETURNS varchar(60)
+DETERMINISTIC
+BEGIN
+    DECLARE counterState int;
+    declare message varchar(60) default 'This user haven\t products in that product state';
+    
+	SELECT sum(user_owns_product.price*users_order_products.user_owns_product_ID) into counterState
+	FROM mydb.users_order_products
+	join mydb.user_owns_product on users_order_products.user_owns_product_ID = users_order_products.ID
+	where users_order_products.user_ID = 1 and users_order_products.product_state_ID = 6;
+    
+    if counterState > 0 then
+		case
+			when _state = 1
+				then set message = concat('Total price for products which lefts store is ', counterState);
+			when _state = 2
+				then set message = concat('Total price for products in warehouse is ', counterState);
+			when _state = 3
+				then set message = concat('Total price for products in way is ', counterState);
+			when _state = 4
+				then set message = concat('Total price for products wich delivered is ', counterState);
+			when _state = 6
+				then set message = concat('Total price for products in busket is ', counterState);
+        end case;
+    end if;
+	
+    RETURN (message);
+END$$
+DELIMITER ;
--- other_sql_script/5 lab/views.sql
+++ other_sql_script/5 lab/views.sql
@@ -1,7 +1,18 @@
+--- 1. user and his comments
+create view `user_comments` as 
+select user.NAME, comments.TEXT from mydb.user
+join mydb.comments on user.ID = comments.user_ID;
 
----example
-CREATE [OR REPLACE]
-[ALGORITHM = {UNDEFINED | MERGE | TEMPTABLE}]
-VIEW view_name [(column_list)]
-AS select_statement
-[WITH [CASCADED | LOCAL] CHECK OPTION]
+
+--- 2. full info about product
+create view `full_info_about_product` as 
+SELECT 
+	products.NAME, 
+    products.DESCRIPTION, 
+    game_series.DESCRIPTION as gameSeriesDescr,
+	published_houses.NAME as pubName, 
+    category.NAME as categoryName
+ FROM mydb.products
+join mydb.game_series on game_series.ID = products.game_series_ID
+join mydb.published_houses on published_houses.ID = products.published_houses_ID
+join mydb.category on category.ID = products.category_ID;
