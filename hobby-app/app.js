f874a9ba5c1f0de16af08524831e57ab94218a5d
Lady_Proger <facktartar@mail.com>
2020-12-14 15:56:24 +0400

    init requests

---
+++ hobby-app/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mysql = require('mysql2')
+const bodyParser = require("body-parser");
+
+const app = express()
+
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+const pool = mysql.createPool({
+    connectionLimit: 5,
+    host: "127.0.0.1",
+    user: "root",
+    database: "mydb",
+    password: "password"
+});
+
+// получение списка пользователей
+app.get("/", function(req, res){
+    pool.query("SELECT * FROM mydb.user;", function(err, data) {
+        if(err) return res.status(500).json({message:err});
+        else return res.status(200).json(data);
+    });
+});
+
+app.listen(process.env.PORT? process.env.PORT : 5000, () => {
+     console.log('App has been started at ' + process.env.DEV_PORT)
+})
