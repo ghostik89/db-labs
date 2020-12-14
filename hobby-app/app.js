2907c13def5fe14d41529a65b3082d570d093cea
Lady_Proger <facktartar@mail.com>
2020-12-14 21:48:38 +0400

    create auth on backend

--- hobby-app/app.js
+++ hobby-app/app.js
@@ -1,27 +1,17 @@
 const express = require('express')
-const mysql = require('mysql2')
 const bodyParser = require("body-parser");
 
 require('dotenv').config()
 const app = express()
 
-const urlencodedParser = bodyParser.urlencoded({extended: false});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-const pool = mysql.createPool({
-    connectionLimit: 5,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: "mydb",
-    password: process.env.DB_PASSWORD
-});
+// parse application/json
+app.use(bodyParser.json())
 
-// получение списка пользователей
-app.get("/", function(req, res){
-    pool.query("SELECT * FROM mydb.user;", function(err, data) {
-        if(err) return res.status(500).json({message:err});
-        else return res.status(200).json(data);
-    });
-});
+app.use('/api/auth', require('./routes/auth.routes'))
+
 
 app.listen(process.env.PORT? process.env.PORT : 5000, () => {
      console.log('App has been started at ' + process.env.PORT)
