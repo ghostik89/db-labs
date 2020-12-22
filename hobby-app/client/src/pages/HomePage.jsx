19084f9de11d3f9e370d47c554024e69ffd67481
Lady_Proger <facktartar@mail.com>
2020-12-23 00:22:54 +0400

    add nav bar to home page

--- hobby-app/client/src/pages/HomePage.jsx
+++ hobby-app/client/src/pages/HomePage.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-
+import {Link} from "react-router-dom"
+import {paths} from "../helpers/paths";
 
 export const HomePage = () => {
     return(
-        <h1>This is a home page</h1>
+        <>
+            <nav>
+                <div className="nav-wrapper">
+                    <Link to={paths.toHome} className="brand-logo">MY HOBBY STORE</Link>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><Link to={paths.toHome}>Sass</Link></li>
+                    </ul>
+                </div>
+            </nav>
+        </>
     )
 }
