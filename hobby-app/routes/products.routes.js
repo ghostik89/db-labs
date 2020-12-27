472685bbc77d14aa5baef9c26cdd9a6c5c3fcdf8
Lady_Proger <facktartar@mail.com>
2020-12-27 12:52:49 +0300

    create get product by id request

--- hobby-app/routes/products.routes.js
+++ hobby-app/routes/products.routes.js
@@ -12,12 +12,25 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD
 });
 
-// /api/products/allProducts
+// /api/products/allProducts - show all products
 router.get('/allProducts', auth, async (req, res) => {
     try{
         pool.query('SELECT * FROM mydb.products;',[],async (err,data) => {
             return err? res.status(500).json({message:err}): res.status(200).json(data);
         })
+    }catch (e) {
+        return res.status(500).json({message: 'Internal server error'})
+    }
+})
+
+// /api/products/product/:id
+router.get('/product/:id', auth, async (req, res) => {
+    try{
+        const id = req.params.id
+        pool.query('select * from `mydb`.`products` where (`ID` = ?);',[id],
+            async (err, data) => {
+                return err? res.status(500).json({message:err}): res.status(200).json(data);
+            })
     }catch (e) {
         return res.status(500).json({message: 'Internal server error'})
     }
