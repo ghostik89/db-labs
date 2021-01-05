a156a5d4c9609c116ee4839f3e11784eb4977399
Lady_Proger <facktartar@mail.com>
2021-01-05 20:06:39 +0300

    beautify header

--- hobby-app/client/src/components/Header/Header.jsx
+++ hobby-app/client/src/components/Header/Header.jsx
@@ -8,12 +8,11 @@ export const Header = () => {
     const {logout} = useContext(AuthContext)
     return(
         <nav>
-            <div className="nav-wrapper">
+            <div className="nav-wrapper purple darken-3 header-container">
                 <Link to={paths.toHome} className="brand-logo">MY HOBBY STORE</Link>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><Link to={paths.toUsersBasketPage}>Корзина</Link></li>
                     <li><Link to={paths.toUserAccountPage}>Аккаунт</Link></li>
-                    <li className={'pointer-btn'} onClick={() => logout()}>Выход</li>
+                    <li className={'pointer-btn'} onClick={() => logout()}><Link to={paths.toLogin}>Выход</Link></li>
                 </ul>
             </div>
         </nav>
